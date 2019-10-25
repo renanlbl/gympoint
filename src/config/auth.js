9ab1f72919df30b438b9a2f8e128974464ede402
renanlbl <cursosprogramacoes@gmail.com>
2019-10-25 10:34:34 -0300

    Adiciona Autenticação de usuário

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ffa2bdec46028129e5936ae2c33b056d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+// import authMiddleware from './app/middlewares/auth';
+
 const routers = new Router();
 
-routers.get('/', (req, res) => {
-  return res.json({ message: 'oi' });
-});
+routers.post('/sessions', SessionController.store);
 
 export default routers;
